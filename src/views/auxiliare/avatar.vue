1f1d8e10be51a0a973aa9d2a3717bea7ebb89e69
JovinMtl <nsanzumukizath@gmail.com>
2024-03-19 10:42:05 +0200

    creating an avatar component

---
+++ src/views/auxiliare/avatar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div style="text-align: center; margin-left: 45vw;">
+                <div style="display: inline-flex; width: 30vw;
+                height: 6vh; 
+                /* background-color: red; */
+                text-align: center;  line-height: 6vh;
+                position:absolute">
+                <div class="avatar" style="
+                    ">
+                    <ion-icon class="iconAvatar" :icon="personCircle"></ion-icon>
+                </div>
+                <div class="username">
+                    {{ store.getters.getUsername }}
+                </div>
+            </div>
+            </div>
+</template>
