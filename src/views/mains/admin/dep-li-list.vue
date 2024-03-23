7b0862ccff04137b177554dd9f7123290ab020aa
JovinMtl <nsanzumukizath@gmail.com>
2024-03-23 12:02:54 +0200

    can view the image and hide it

--- src/views/mains/admin/dep-li-list.vue
+++ src/views/mains/admin/dep-li-list.vue
@@ -1,9 +1,43 @@
 <template>
     <div style="text-align: center;">
         <p>Here we see depot, we want to view an image</p>
-        <img src="http://127.0.0.1:8002/depots/image_1zmrQZT.png" 
-            alt="Depot2">
+        <img src="http://127.0.0.1:8002/prof/depots/image_RKJV27N.jpeg" 
+            alt="Depot2" v-show="allowImage">
+        <ion-list>
+            <ion-item>
+                Depot, <ion-thumbnail
+                        @click="turnImage">
+                    <img src="http://127.0.0.1:8002/prof/depots/image_RKJV27N.jpeg"
+                            alt="Bordereau"  style="border-radius: 15px;
+                            border: 1px solid orange;"/>
+                </ion-thumbnail>
+            </ion-item>
+        </ion-list>
 
     </div>
     
 </template>
+
+<script>
+import { IonThumbnail, IonList, IonItem, } from '@ionic/vue'
+import { ref, } from 'vue'
+import { all } from 'axios'
+export default {
+    components: {
+        IonThumbnail, IonList, IonItem, 
+    },
+    setup() {
+        const allowImage = ref(false)
+
+        function turnImage(){
+            allowImage.value = !allowImage.value
+        }
+
+        return {
+            allowImage,
+            turnImage,
+        }
+        
+    },
+}
+</script>
