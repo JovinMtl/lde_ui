e1ef1323d07f5fc7d94c3cf8e4e868870e99b848
JovinMtl <nsanzumukizath@gmail.com>
2024-03-23 12:45:25 +0200

    can fetch on Depot

--- src/views/mains/admin/dep-li-list.vue
+++ src/views/mains/admin/dep-li-list.vue
@@ -21,21 +21,82 @@
 <script>
 import { IonThumbnail, IonList, IonItem, } from '@ionic/vue'
 import { ref, } from 'vue'
-import { all } from 'axios'
+import { useStore } from 'vuex'
 export default {
     components: {
         IonThumbnail, IonList, IonItem, 
     },
     setup() {
+        const store = useStore()
         const allowImage = ref(false)
 
         function turnImage(){
             allowImage.value = !allowImage.value
         }
+        const message = ref(null)
+        const operationSuccess = ref(false)
+        
+        const allDepots = ref(null)
+        var suffix = '/jov/api/depot/getDepotAll/'
+
+        async function kubaza(){
+            const url = ' http://localhost:8002/jov/api/invest/allInvests/'
+            const baseURL = '//127.0.0.1:8002'
+            
+
+            try {
+                const response = await fetch(`${baseURL}${suffix}`,{
+                    method: 'GET',
+                    headers:{
+                        // 'Content-type' : 'application/json',
+                        'Authorization' : 'Bearer '+ store.getters.getAccessToken,
+                    },
+                });
+
+                if (response.ok){
+                    allDepots.value = await response.json()
+                    console.log("Things are well received")
+                    // console.log(allInvests.value)
+                } else {
+                    console.log("Connection wasn't successfull, with : ", store.getters.getAccessToken)
+                }
+            } catch(value){
+                console.log("THe reason is : ", value)
+            }
+        }
+
+        async function kwemeza(link){
+            console.log("The click element: ", link)
+
+
+            try{
+                const responseActivate = await fetch(`${link}`,{
+                    method: 'GET',
+                        headers:{
+                            'Authorization' : 'Bearer '+ store.getters.getAccessToken,
+                        },
+                })
+                if(responseActivate.ok){
+                    console.log("Vous avez bien approuvee cet investissement ")
+                    message.value = "Vous avez bien approuvee cet investissement "
+                    operationSuccess.value = true
+                } else{
+                    console.log("Cet investissement n'est pas approuvee")
+                    message.value = "Cet investissement n'est pas approuvee"
+                    operationSuccess.value = false
+                }
+            } catch(value){
+                operationSuccess.value = false
+                message.value = "Investissement non approuve, probleme de connexion."
+            }
+            
+        }
+        kubaza()
 
         return {
             allowImage,
             turnImage,
+            allDepots,
         }
         
     },
