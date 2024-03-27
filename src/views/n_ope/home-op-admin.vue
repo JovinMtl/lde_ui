a0ffb0790d4e4a92201892cf0288b2b6791639c5
JovinMtl <nsanzumukizath@gmail.com>
2024-03-27 14:00:18 +0200

    added accent on verifier

--- src/views/n_ope/home-op-admin.vue
+++ src/views/n_ope/home-op-admin.vue
@@ -42,7 +42,7 @@
             <ion-label id="lab1">Accueil</ion-label>
             <ion-label id="lab2">Historique</ion-label>
             <!-- <ion-label id="lab3">Portefeuille</ion-label> -->
-            <ion-label id="lab4">Verifier</ion-label>
+            <ion-label id="lab4">Vérifier</ion-label>
         </template>
             
     </base-menu>
