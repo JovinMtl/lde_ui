c7d70faf12f63b3da15e9b8a520b86a44d012d81
JovinMtl <nsanzumukizath@gmail.com>
2024-04-03 16:14:55 +0200

    deleting buttons on 'yet approved retrait'

--- src/views/mains/admin/ret-li-yp.vue
+++ src/views/mains/admin/ret-li-yp.vue
@@ -9,116 +9,25 @@
                 >
                 Retrait, {{ (retrait.date_submitted).slice(11,16) }};
                 {{ retrait.owner }} <==> {{ retrait.montant }} ({{ retrait.currency }})
-                
-                <span v-show="!retrait.approved" style="display: flex;" >
-                <button  :id="'jo'+index" :value="retrait.link_to_approve" 
-                    @click="kwemeza($event.target)"
-                    style="height: 2em; margin: 5px 5px; background-color: orange;
-                            color: black;border-radius: 5px; padding: 0 5px;">
-                    Approuver
-                </button>
-                <button  :id="'jo'+index" :value="retrait.link_to_approve" 
-                    @click="kwemeza($event.target.value)"
-                    style="height: 2em; margin: 5px 5px; background-color: red;
-                            color: black;border-radius: 5px; padding: 0 5px;">
-                    Refuser
-                </button>
-            </span>
             </ion-item>
         </ion-list>
-        <div v-show="waiting" style="background-color: transparent; position:fixed; 
-                top: 60%; left: 50%; transform: translate(-50%, -50%);
-                z-index: 5;">
-            <!-- <ion-spinner color="danger" name="lines-sharp"></ion-spinner> -->
-            <loa-der></loa-der>
-        </div>
     </div>
     
 </template>
 
 <script>
 import { 
-    IonThumbnail, IonList, IonItem, IonButton, IonAlert, IonSpinner,
-    alertController,
+    IonList, IonItem,
 } from '@ionic/vue'
 import { ref } from 'vue'
-import { useStore } from 'vuex'
-import { useRouter} from 'vue-router'
-import Loader from '../../auxiliare/processing/processing1.vue'
-// import emptyModal from '../emptyModal.vue'
+import { useStore} from 'vuex'
 export default {
-    components: {
-        'loa-der': Loader,
-        'alert-controller': alertController,
-        IonThumbnail, IonList, IonItem, IonButton, IonAlert, IonSpinner,
+    components:{
+        IonList, IonItem,
     },
     setup() {
         const store = useStore()
-        const router = useRouter()
-        const allowImage = ref(false)
-        const link = ref(null)
-        const waiting = ref(false)
-        const networkFailled = ref(false)
-        const tokenExpired = ref(false)
-
-        const modalActive = ref(false)
-        const alertButtons = ['Ok'];
-
-        function toogleModal(value){
-            modalActive.value = value
-        }
-
-        const presentAlert = async () => {
-    const alert = await alertController.create({
-        header: 'Opération réussie',
-        message: 'Vous avez bien approuvé <strong>cette</strong> recharge.',
-        buttons: ['Ok'],
-        mode: 'ios',
-        backdropDismiss: false
-    });
-
-    // Displaying the alert with the correct HTML rendering
-    await alert.present().then(() => {
-        const element = document.querySelector('ion-alert .alert-message');
-        if (element) {
-            element.innerHTML = element.textContent;
-        }
-    });
-};
-        const tokenExpiredAlert = async () => {
-            const alert = await alertController.create({
-            header: 'Operation echoue',
-            message: 'Veuillez vous reconnecter encore.',
-            buttons: ['Ok'],
-            });
-
-            await alert.present();
-        };
-        const networkFailledAlert = async () => {
-            const alert = await alertController.create({
-            header: 'Operation echoue',
-            // subHeader: 'A Sub Header Is Optional',
-            message: 'Probleme de connexion au serveur.',
-            buttons: ['Ok'],
-            });
-
-            await alert.present();
-        };
-
-        function turnImage(value){
-            allowImage.value = !allowImage.value
-            console.log("The link is : ", value)
-            store.commit('setActualBordereau', value)
-            link.value = value
-            console.log("The committed value : ", store.getters.getActualBordeau)
-            router.push('/oimage')
-        }
-        // const message = ref(null)
-        const operationSuccess = ref(false)
-        
         const allRetraits = ref(null)
-        
-        var indexApproved = 0
 
         async function kubaza(){
             console.log("RET-LI-LIST: Begin to Ask")
@@ -147,69 +56,12 @@ export default {
             }
         }
 
-        async function kwemeza(link){
-            waiting.value = true
-            indexApproved = Number(link.id.slice(2))
-            
-            try{
-                const responseActivate = await fetch(`${link.value}`,{
-                    method: 'GET',
-                        headers:{
-                            'Authorization' : 'Bearer '+ store.getters.getAccessToken,
-                        },
-                })
-                if(responseActivate.ok){
-                    const data = await responseActivate.json()
-                    console.log("Vous avez bien approuvee cette Recharge ", data)
-                    allDepots.value.valueOf(5)[indexApproved].approved = true
-                    
-                    
-                    setTimeout(()=>{
-                        console.log("The result is okay, waiting: ", waiting.value)
-                        waiting.value = false
-                        // modalActive.value = true
-                        // operationSuccess.value = true
-                        presentAlert()
-                    }, 2000)
-                } else{
-                    console.log("Cette RECHARGE n'est pas approuvee")
-                    // operationSuccess.value = false
-                    modalActive.value = false
-                    tokenExpiredAlert()
-                    // waiting.value = false
-                    // tokenExpired.value = true
-                }
-            } catch(value){
-                operationSuccess.value = false
-                modalActive.value = false
-                networkFailledAlert()
-                // tokenExpired.value = false
-                // networkFailled.value = true
-            }
-            
-        }
         kubaza()
 
         return {
-            allowImage, link,
-            turnImage, kwemeza, toogleModal,
             allRetraits,
-            operationSuccess,
-            modalActive,
-            alertButtons, 
-            presentAlert, tokenExpiredAlert, networkFailledAlert,
-            waiting,
-            networkFailled, tokenExpired, 
         }
         
     },
 }
 </script>
-
-<style scoped>
-.fermer{
-    /* background: red; */
-    color: green;
-    transform: translate(-1%, 230%);
-}
-</style>
