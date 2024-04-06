b0c741be4b14cfdc2ab78e1edc37605267014f57
JovinMtl <nsanzumukizath@gmail.com>
2024-04-06 14:33:32 +0200

    setting a new signature which will feet to the footer

---
+++ src/views/auxiliare/signature-footer.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div id="endPag">
+        <div id="logolit"></div>
+        <p style="text-shadow: 0 0 10px black;">LD Lit Dinar </p>
+    </div>
+</template>
+<style scoped>
+    #endPag{
+            display: inline-flex;
+            position: fixed;
+            width: 100%;
+            height: 20px;
+            /* bottom: 75px; */
+            bottom : 12vh;
+            justify-content: center;
+            text-align: center;
+        }
+    #logolit{
+        display: inline-flex;
+        position: relative;
+        width: 40px;
+        height: 40px;
+        top: 5px;
+        margin-right: 10px;
+        /* background-image: url('../../public/resources/logo/logo_home/Lit_Dinard_ON_ORANGE.png'); */
+        background-image: url('/Lit_Dinard_ON_GRAY.png');
+        background-size: cover;
+    }
+    
+</style>
